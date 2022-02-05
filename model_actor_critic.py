c723a2ee3d6c5d338b994403d6429c2945ea6fcb
enes3774 <77508537+enes3774@users.noreply.github.com>
2022-02-05 15:27:02 +0300

    Update model_actor_critic.py

--- model_actor_critic.py
+++ model_actor_critic.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.layers import Dense, Input, Concatenate, BatchNormalization, Activation, LeakyReLU
+from tensorflow.keras.layers import Dense, Input, Concatenate, BatchNormalization, Activation, LeakyReLU,Conv2D,Flatten
 from tensorflow.keras import Model
 from tensorflow.keras import regularizers
 
@@ -12,7 +12,7 @@ warnings.filterwarnings("ignore")
 class Actor:
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, fc1_units=128, fc2_units=64, name="Actor"):
+    def __init__(self, state_size, action_size, fc1_units=1024, fc2_units=128, name="Actor"):
         """Initialize parameters.
 
         Params
@@ -36,10 +36,16 @@ class Actor:
     def build_model(self):
         """Build an actor (policy) network that maps states -> actions."""
         # Define input layer (states)
-        states = Input(shape=(self.state_size,))
-        
+        states = Input(shape=(self.state_size))
+        conv_states=Conv2D(32, kernel_size=4, strides = (2,2), input_shape = (4,88,192),padding = "same")(states)
+        conv_activation=Activation("relu")(conv_states)
+        conv_states=Conv2D(64,kernel_size=4,strides=(2,2),padding="same")(conv_activation)
+        conv_activation=Activation("relu")(conv_states)
+        conv_states=Conv2D(64,kernel_size=3,strides=(1,1),padding="same")(conv_activation)
+        conv_activation=Activation("relu")(conv_states)
+        flatten=Flatten()(conv_activation)
         # Add hidden layers
-        net = Dense(units=self.fc1_units, activation='relu', kernel_initializer='glorot_uniform')(states)
+        net = Dense(units=self.fc1_units, activation='relu', kernel_initializer='glorot_uniform')(flatten)
         net = Dense(units=self.fc2_units, activation='relu', kernel_initializer='glorot_uniform')(net)
 
         # Add final output layer with tanh activation
@@ -54,7 +60,7 @@ class Actor:
 class Critic:
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, fc1_units=128, fc2_units=64, name="Critic"):
+    def __init__(self, state_size, action_size, fc1_units=1024, fc2_units=128, name="Critic"):
         """Initialize parameters and build model.
 
         Params
@@ -79,11 +85,20 @@ class Critic:
     def build_model(self):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         # Define input layers
-        states = Input(shape=(self.state_size,))
+        states = Input(shape=(self.state_size))
         actions = Input(shape=(self.action_size,))
 
         # Add hidden layer for state pathway
-        net_states = Dense(units=self.fc1_units, activation='relu', kernel_initializer='glorot_uniform')(states)
+        conv_states=Conv2D(32, kernel_size=4, strides = (2,2), input_shape = (4,88,192),padding = "same")(states)
+        conv_activation=Activation("relu")(conv_states)
+        conv_states=Conv2D(64,kernel_size=4,strides=(2,2),padding="same")(conv_activation)
+        conv_activation=Activation("relu")(conv_states)
+        conv_states=Conv2D(64,kernel_size=3,strides=(1,1),padding="same")(conv_activation)
+        conv_activation=Activation("relu")(conv_states)
+        flatten=Flatten()(conv_activation)
+        
+        
+        net_states = Dense(units=self.fc1_units, activation='relu', kernel_initializer='glorot_uniform')(flatten)
 
         # Combine state and action pathways
         net = Concatenate(axis=-1)([net_states, actions])
